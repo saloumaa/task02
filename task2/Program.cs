f9a8540662fb1b1d04ec208e1df0361b1faead83
salma mohamed <salmamahmed091@gmail.com>
2025-08-15 19:07:57 +0300

    commit1

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,6 +1,6 @@
-﻿int[] numbers= new int[100];
-int count = 0; 
+﻿List<int> mylist = new List<int>();
 string choice;
+
 do
 {
     Console.WriteLine("P - Print numbers");
@@ -8,79 +8,122 @@ do
     Console.WriteLine("M - Display mean of the numbers");
     Console.WriteLine("S - Display the smallest number");
     Console.WriteLine("L - Display the largest number");
+    Console.WriteLine("F - Find a number");
+    Console.WriteLine("C - Clear the list");
     Console.WriteLine("Q - Quit");
     Console.WriteLine("===============================");
     Console.Write("Enter your choice: ");
     choice = Console.ReadLine().ToLower();
+
     switch (choice)
     {
         case "p":
-            if (count == 0)
+            if (mylist.Count == 0)
             {
-                Console.WriteLine("[] - the array is empty");
-
+                Console.WriteLine("[] - the list is empty");
             }
             else
             {
                 Console.Write("[");
-                for (int i = 0; i < count; i++)
+                for (int i = 0; i < mylist.Count; i++)
                 {
-                    Console.Write(numbers[i]);
-                    if (i < count - 1)
+                    Console.Write(mylist[i]);
+                    if (i < mylist.Count - 1)
                         Console.Write(" ");
                 }
-                Console.Write("]");
-
-
+                Console.WriteLine("]");
             }
             break;
+
         case "a":
             Console.Write("Enter an integer to add: ");
             int num = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"{num}added");
+            mylist.Add(num);
+            Console.WriteLine($"{num} added");
             break;
+
         case "m":
-            if (count == 0)
+            if (mylist.Count == 0)
             {
                 Console.WriteLine("Unable to calculate the mean - no data");
             }
             else
             {
                 int sum = 0;
-                for (int i = 0; i < count; i++)
-                    sum += numbers[i];
-                double mean = (double)sum / count;
-                Console.WriteLine($"{mean}");
+                for (int i = 0; i < mylist.Count; i++)
+                    sum += mylist[i];
+                double mean = (double)sum / mylist.Count;
+                Console.WriteLine($"{mean:F2}");
             }
             break;
+
         case "s":
-            if (count == 0) { Console.WriteLine("Unable to determine the smallest number - array is empty"); }
+            if (mylist.Count == 0)
+            {
+                Console.WriteLine("Unable to determine the smallest number - list is empty");
+            }
             else
             {
-                int smallest = numbers[0];
-                for (int i = 1; i < count; i++)
-                    if (numbers[i] < smallest)
-                        smallest = numbers[i];
-                Console.WriteLine($"{smallest}");
+                int smallest = mylist[0];
+                for (int i = 1; i < mylist.Count; i++)
+                    if (mylist[i] < smallest)
+                        smallest = mylist[i];
+                Console.WriteLine(smallest);
             }
             break;
+
         case "l":
-            if (count == 0) { Console.WriteLine("Unable to determine the largest number - array is empty"); }
+            if (mylist.Count == 0)
+            {
+                Console.WriteLine("Unable to determine the largest number - list is empty");
+            }
             else
             {
-                int largest = numbers[0];
-                for (int i = 1; i < count; i++)
-                    if (numbers[i] > largest)
-                        largest = numbers[i];
-                Console.WriteLine($" {largest}");
+                int largest = mylist[0];
+                for (int i = 1; i < mylist.Count; i++)
+                    if (mylist[i] > largest)
+                        largest = mylist[i];
+                Console.WriteLine(largest);
             }
             break;
+
+        case "f":
+            if (mylist.Count == 0)
+            {
+                Console.WriteLine("List is empty");
+            }
+            else
+            {
+                Console.Write("Enter number to search: ");
+                int searchNum = Convert.ToInt32(Console.ReadLine());
+                bool found = false;
+                for (int i = 0; i < mylist.Count; i++)
+                {
+                    if (mylist[i] == searchNum)
+                    {
+                        Console.WriteLine($"{searchNum} found at index {i}");
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    Console.WriteLine($"{searchNum} not found");
+                }
+            }
+            break;
+
+        case "c":
+            mylist.Clear();
+            Console.WriteLine("List cleared");
+            break;
+
         case "q":
             Console.WriteLine("Goodbye");
             break;
+
         default:
             Console.WriteLine("Unknown selection, please try again");
             break;
-
-    } 
-} while (choice != "q"); 
+    }
+} while (choice != "q");
